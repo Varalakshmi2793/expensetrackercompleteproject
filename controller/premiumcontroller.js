5e1bba038752179e42913b68895fa006d12cb0c9
Varalakshmmi2793 <varaluckky.2@gmail.com>
2024-04-29 11:29:58 +0530

    delete expense

--- controller/premiumcontroller.js
+++ controller/premiumcontroller.js
@@ -2,18 +2,19 @@ const User = require('../model/user');
 const Expense = require('../model/tracker');
 const sequelize = require('../path/database');
 
-
 exports.getLeaderboard = async (req, res) => {
     try {
         console.log('Fetching leaderboard data...');
-        const usersleaderboard = await User.findAll({
+        const usersLeaderboard = await User.findAll({
             attributes: [
                 'id',
                 'username',
-                'totalexpense']})
-    console.log(usersleaderboard);
-
-        res.status(200).json({ success: true, leaderboard: usersleaderboard });
+                'totalexpense'
+            ],
+            order: [['totalexpense', 'DESC']]
+        });
+        console.log(usersLeaderboard);
+        res.status(200).json({ success: true, leaderboard: usersLeaderboard });
     } catch (err) {
         console.error(err);
         res.status(500).json({ success: false, message: 'Internal server error' });
