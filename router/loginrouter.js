df1ee2562205684ffd909baa5fc8a1133eb2afea
Varalakshmi2793 <163169199+Varalakshmi2793@users.noreply.github.com>
2024-05-01 23:32:19 +0530

    completeproject

--- router/loginrouter.js
+++ router/loginrouter.js
@@ -7,14 +7,4 @@ const router = express.Router();
 router.post('/signup', controller.signupform);
 router.post('/login', controller.loginform);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 96a6eeb49bd73a14325de3b68195ea4a4e6c3975
-=======
->>>>>>> 96a6eeb49bd73a14325de3b68195ea4a4e6c3975
-=======
->>>>>>> 96a6eeb49bd73a14325de3b68195ea4a4e6c3975
 module.exports = router; 
